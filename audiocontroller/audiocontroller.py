f266da4761233eeab2540561e6bb1fc545a175b0
Ken Bromberg <kennyb@alltooflat.com>
2018-05-18 01:19:11 +0000

    Moving worker and queue into audiocontroller

--- audiocontroller/audiocontroller.py
+++ audiocontroller/audiocontroller.py
@@ -3,15 +3,50 @@ import pygame
 import Queue
 
 
+audio_queue = Queue.Queue()
+
+def audio_controller_worker(config):
+    """
+    config is an array of dict objects:
+
+    [ {'name': 'piano', 'sound': 'piano2.wav', 'loopable': False } ]
+
+    """
+    ac = AudioController(config)
+    
+    while True:
+        soundInfo = audio_queue.get(block=True)
+        logging.debug("audioQueue.get pulled [%s]" % str(soundInfo) )
+
+        if dict is not type(soundInfo):
+            logging.warn("audioQueue.get pulled an object that was not a dict")
+            continue
+        
+        try:
+            if soundInfo['action'] == 'play':
+                ac.playSound(soundInfo['name'], soundInfo['loop'])
+            elif soundInfo['action'] == 'stop':
+                ac.stopSound(soundInfo['name'])
+            elif soundInfo['action'] == 'end_thread':
+                return # special message to end the thread
+        except KeyError:
+            logging.error("KeyError - soundInfo %s improperly structured" %
+                            soundInfo)
+            
+
+
 class AudioController:
-    def __init__(self):
+    def __init__(self, config):
         pygame.init()
         pygame.mixer.init()
 
         self._audio_registry = {}
+        for item in config:
+            self.__register(item['name'], item['sound'], item['loopable'])
 
-    def register(self, registry_name, file_path, loopable=False):
-        # TODO: Check for collision here
+
+    
+    def __register(self, registry_name, file_path, loopable=False):
         self._audio_registry[registry_name] = {
             'sound': pygame.mixer.Sound(file_path),
             'loopable': loopable
--- audiocontroller/audiocontroller_test.py
+++ audiocontroller/audiocontroller_test.py
@@ -1,50 +1,19 @@
-import Queue
-from audiocontroller import AudioController
-
-audioQueue = Queue.Queue()
-
-"""
-def worker():
-    while True:
-        item = q.get()
-        do_work(item)
-        q.task_done()
-
-q = Queue()
-for i in range(num_worker_threads):
-     t = Thread(target=worker)
-     t.daemon = True
-     t.start()
-
-for item in source():
-    q.put(item)
-
-q.join()       # block until all tasks are done
-"""
+import threading
+from audiocontroller import audio_queue, audio_controller_worker
+from audiocontroller import AudioController as ac
+import time
 
 
-"""
-soundInfo = { 'name': <registry_name>,
-              'action': <play|stop>,
-              'loop': <True|False>
-            }
-"""
+config = [ {'name': 'piano', 'sound': 'piano2.wav', 'loopable': False } ]
+cmd = {'name': 'piano', 'action': 'play', 'loop': False}
+
+t = threading.Thread(target=audio_controller_worker, args=[config])
+t.start()
 
 
-def audio_worker():
-    ac = AudioController()
-    ac.register(registry_name='piano', file_path='piano2.wav')
+time.sleep(1)
+audio_queue.put(cmd)
+time.sleep(5)
 
-    while True:
-        soundInfo = None
-        try:
-            soundInfo = audioQueue.get()
-        except Queue.Empty:
-            pass
-
-        if soundInfo:
-            print soundInfo
-            if soundInfo['action'] == 'play':
-                ac.playSound(soundInfo['name'], soundInfo['loop'])
-            elif soundInfo['action'] == 'stop':
-                ac.stopSound(soundInfo['name'])
+end_thread = { 'action': 'end_thread' }
+audio_queue.put(end_thread)
